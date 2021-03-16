001ea9b0290a0042620a3f830fb81b4893c26b75
dayong <dayong@conflux-chain.org>
2021-03-16 11:41:41 +0800

    remove unused functions

--- packages/web3-providers-http-proxy/src/confluxUtil.js
+++ packages/web3-providers-http-proxy/src/confluxUtil.js
@@ -1,51 +1,21 @@
 const util = require('./util');
 const yargs = require("yargs/yargs");
 
-// not work
-function syncFunc(asyncFunc) {
-  return function() {
-    var sync = true;
-    var data = null;
-    console.log("asyncFunc start.");
-    // setTimeout(() => {
-    asyncFunc(...arguments).then(config => {
-      console.log("asyncFunc done.", config);
-      if (err) throw err;
-      sync = false;
-      data = config;
-    });
-    // sync = false;
-    // }, 2000);
-
-    while (sync) {
-      require("deasync").sleep(100);
-      console.log("sleep", sync);
-    }
-
-    return data;
-  };
-}
 
 async function detectNetworkId() {
-  // console.log("start detect config");
   const { Environment } = require("@truffle/environment");
   const Config = require("@truffle/config");
   
   const inputArguments = process.argv.slice(2);
   let args = yargs();
   let argv = args.parse(inputArguments);
+  
   const config = Config.detect({network:argv.network});
-  // console.trace("config.network",config.network);
   await Environment.detect(config);
-  // console.trace(config.networks[config.network].network_id);
   return config.networks[config.network].network_id;
 }
-
-let detectNetowrkIdSync = syncFunc(detectNetworkId);
-// let detectConfigSync = syncFunc(async () => { return { networkId: 10 }; });
 
 module.exports = {
   ...util,
   detectNetworkId,
-  detectNetowrkIdSync
 };
